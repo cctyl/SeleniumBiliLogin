aef9a2e89bccc9d88bdd601f4e56f199aaf41fcd
cctyl <34262992+cctyl@users.noreply.github.com>
2019-07-24 11:09:33 +0800

    固定了窗口大小
    本程序会因为窗口大小的不同而导致登录失败，目前可能需要针对不同大小的屏幕单独适配，这里的大小是我适配时的屏幕大小，可以测试通过

--- src/main/java/cn/tyl/selenium/SeleniumBiliLogin.java
+++ src/main/java/cn/tyl/selenium/SeleniumBiliLogin.java
@@ -1,7 +1,9 @@
-package cn.tyl.selenium;
+package cn.tyl.utils;
 
+import cn.tyl.domain.User;
 import cn.tyl.utils.SeleniumUtil;
 import org.apache.commons.lang3.RandomUtils;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -14,13 +16,16 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 
 /**
  * selenium破解极验滑动验证码
  */
-public class SeleniumBiliLogin {
+public class BiliLogin {
+
+    public static Logger log4j =  Logger.getLogger(BiliLogin.class);
+
+
     private static String BASE_PATH = "";
     //开始遍历处距离左边的距离
     private static final int GEETEST_WIDTH_START_POSTION = 60;
@@ -30,31 +35,50 @@ public class SeleniumBiliLogin {
     private static Point imageFullScreenSize = null;
     //html 大小
     private static Point htmlFullScreenSize = null;
-    static {
-//        System.setProperty("webdriver.chrome.driver", "/Users/wangyang/Downloads/chromedriver");
-        driver = new ChromeDriver();
+
+    public static void login(WebDriver oldDriver) throws InterruptedException {
+
+
+        driver = (ChromeDriver) oldDriver;
+
+        loginNext();
+
     }
-    public static void main(String[] args) throws InterruptedException {
+
+    //    static {
+////        System.setProperty("webdriver.chrome.driver", "/Users/wangyang/Downloads/chromedriver");
+//        driver = new ChromeDriver();
+//    }
+    public static void loginNext() throws InterruptedException {
+
+
+        log4j.info("开始执行loginNext方法");
         try {
             for(int i = 0; i < 1; i++){
-//                driver.manage().window().setSize(new Dimension(1024, 768));
-                driver.manage().window().maximize();
+                log4j.info("设置窗口大小");
+                driver.manage().window().setSize(new Dimension(1855, 937));
+                log4j.info("打开登录页面");
                 driver.get("https://passport.bilibili.com/login");
-
+                User u = new User();
                 WebElement username = driver.findElement(By.id("login-username"));
                 WebElement passwd = driver.findElement(By.id("login-passwd"));
-                SeleniumUtil.write(username,"你的帐号");
-                SeleniumUtil.write(passwd,"你的密码");
+                SeleniumUtil.write(username,u.getUsername());
+                SeleniumUtil.write(passwd,u.getPassword());
+                log4j.info("输入密码，点击登录按钮");
                 driver.findElement(By.xpath("//li[@class='btn-box']/a[1]")).click();//找到登录按钮并且点击
+
 
                 Thread.sleep(3 * 1000);
                 Actions actions = new Actions(driver);
+
+                log4j.info("获取验证码图片slider.png");
                 //图一
                 BufferedImage image = getImageEle(driver.findElement(By.cssSelector("canvas[class='geetest_canvas_bg geetest_absolute']")));
                 ImageIO.write(image, "png",  new File(BASE_PATH + "slider.png"));
                 //设置原图可见
                 driver.executeScript("document.getElementsByClassName(\"geetest_canvas_fullbg\")[0].setAttribute('style', 'display: block')\n");
                 //图二
+                log4j.info("获取验证码图片original.png");
                 image = getImageEle(driver.findElement(By.cssSelector("canvas[class='geetest_canvas_fullbg geetest_fade geetest_absolute']")));
                 ImageIO.write(image, "png",  new File(BASE_PATH + "original.png"));
                 //隐藏原图
@@ -62,26 +86,29 @@ public class SeleniumBiliLogin {
                 WebElement element = null;
                 element = driver.findElement(By.className("geetest_slider_button"));
                 actions.clickAndHold(element).perform();
+
                 int moveDistance = calcMoveDistance();
+                log4j.info("计算移动距离"+moveDistance+"，开始移动");
                 int d = 0;
 
                 List<MoveEntity> list = getMoveEntity(moveDistance+6);
-                System.out.println(list.size());
+                log4j.info(list.size());
                 for(MoveEntity moveEntity : list){
                     actions.moveByOffset(moveEntity.getX(), moveEntity.getY()).perform();
-                    System.out.println("向右总共移动了:" + (d = d + moveEntity.getX()));
+                    log4j.debug("向右总共移动了:" + (d = d + moveEntity.getX()));
                     Thread.sleep(moveEntity.getSleepTime());
                 }
                 actions.release(element).perform();
                 Thread.sleep(1 * 1000);
             }
+            log4j.info("登录完毕");
 
         } catch (Exception e) {
-            e.printStackTrace();
+            log4j.error("loginNext出错了，错误为-----"+e.getMessage());
         } finally {
-            System.out.println("测试完毕，将在10秒内关闭浏览器");
+
             Thread.sleep(10000);
-            driver.quit();
+
         }
     }
 
@@ -91,17 +118,18 @@ public class SeleniumBiliLogin {
      * @return
      */
     private static BufferedImage getImageEle(WebElement ele) {
+        log4j.info("获取图片");
         try {
             byte[] fullPage = driver.getScreenshotAs(OutputType.BYTES);
             BufferedImage fullImg = ImageIO.read(new ByteArrayInputStream(fullPage));
-            System.out.println("fullImage: width:" + fullImg.getWidth() + ", height:" + fullImg.getHeight());
+            log4j.info("fullImage: width:" + fullImg.getWidth() + ", height:" + fullImg.getHeight());
             ImageIO.write(fullImg, "png",  new File(BASE_PATH + "full.png"));
             if (imageFullScreenSize == null){
                 imageFullScreenSize = new Point(fullImg.getWidth(), fullImg.getHeight());
             }
             WebElement element = driver.findElement(By.cssSelector("div[id='login-app']"));
 
-            System.out.println("html: width:" + element.getSize().width + ", height:" + element.getSize().height);
+            log4j.info("html: width:" + element.getSize().width + ", height:" + element.getSize().height);
             if(htmlFullScreenSize == null){
                 htmlFullScreenSize = new Point(1855, 937);
 //                htmlFullScreenSize = new Point(element.getSize().getWidth(), element.getSize().getHeight());
@@ -122,9 +150,9 @@ public class SeleniumBiliLogin {
             float elementWidth = (float)element.getSize().width;
             float fullImgWidth  = (float)fullImg.getWidth();
 
-            System.out.println("pointX--"+pointx + "--elementWidth--"+elementWidth+"--fullImgWidth--"
+            log4j.debug("pointX--"+pointx + "--elementWidth--"+elementWidth+"--fullImgWidth--"
                     +fullImgWidth);
-            System.out.println("因此 x坐标为："+(int)point.getX()+"除以"+(float)element.getSize().width +"乘以"+ (float)fullImg.getWidth()+"="+(int)(point.getX() / (float)element.getSize().width * (float)fullImg.getWidth()));
+            log4j.debug("因此 x坐标为："+(int)point.getX()+"除以"+(float)element.getSize().width +"乘以"+ (float)fullImg.getWidth()+"="+(int)(point.getX() / (float)element.getSize().width * (float)fullImg.getWidth()));
 
 
 
@@ -153,7 +181,7 @@ public class SeleniumBiliLogin {
 
 
 
-            System.out.println("x--"+(int)(point.getX() / (float)element.getSize().width * (float)fullImg.getWidth())+"" +
+            log4j.debug("x--"+(int)(point.getX() / (float)element.getSize().width * (float)fullImg.getWidth())+"" +
                     "  y--"+(int)(point.getY() / (float)element.getSize().height * (float)fullImg.getHeight())+"" +
                     "要截取的宽："+eleWidth+" --要截取的高："+eleHeight);
 
@@ -245,6 +273,7 @@ public class SeleniumBiliLogin {
      * @return
      */
     private static int calcMoveDistance() {
+        log4j.info("开始计算距离");
         //小方块距离左边界距离
         int START_DISTANCE = 6;
         int startWidth = (int)(GEETEST_WIDTH_START_POSTION * (imageFullScreenSize.x + 0.0f)/ htmlFullScreenSize.x);
@@ -265,7 +294,7 @@ public class SeleniumBiliLogin {
                     bgRgb[2] = (geetest2.getRGB(i, j)  & 0xff);
                     if(difference(fullRgb, bgRgb) > 255){
                         int moveDistance = (int)((i - START_DISTANCE) / ((imageFullScreenSize.x + 0.0f)/ htmlFullScreenSize.x));
-                        System.out.println("需要移动的距离:" + moveDistance);
+                        log4j.debug("需要移动的距离:" + moveDistance);
                         return moveDistance;
                     }
                 }
